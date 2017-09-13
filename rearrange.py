6c173468824de344dd0405d1b029046c1f8ab28d
Specialist17 <arocho.fernando.13@gmail.com>
2017-09-12 20:55:39 -0700

    Mad lib
    the strings are not working on the terminal

--- rearrange.py
+++ rearrange.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import random
 import sys
 
@@ -24,11 +25,25 @@ print(sentence)
 # a[:]              # a copy of the whole array
 # a[start:end:step] # start through not past end, by step
 
-# That's the same as a[:] a copy of the whole list, with a :step value of -1, which results in a reversed copy of the whole list
+# That's the same as a[:] a copy of the whole list, with a :step value of -1,
+# which results in a reversed copy of the whole list
 print(sentence[::-1])
 
 
 print(''.join(reversed(sentence)))
 
 # An interactive "mad libs" script
+
+
+number = input("Number: ")
+profession1 = str(input("Enter a profession: "))
+profession2 = input("Enter another profession: ")
+city1 = input("Enter a city: ")
+name = input("Enter a name: ")
+city2 = input("Enter another city: ")
+profession3 = input("Enter another profession: ")
+bodypart = input("Enter a body part: ")
+
+print(number + " years after the end of Rush Hour 2, James Carter is no longer a " + profession1 + ", but a " + profession2 + " on the streets of " + city1 + ". Lee is now the bodyguard for his friend " + name + ". Lee is still upset with Carter about an incident in " + city2 + " when Carter accidentally shot Lee`s girlfriend, " + profession3 + "Isabella Molina, in the " + bodypart + ".")
+
 # Anagram generator
