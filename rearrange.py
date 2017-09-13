f7136f04dbdd3a3f06736c1ee21ac1ae7d4ffbd9
Specialist17 <arocho.fernando.13@gmail.com>
2017-09-12 20:28:38 -0700

    string reversal

--- rearrange.py
+++ rearrange.py
@@ -14,3 +14,21 @@ print(sentence)
 # if __name__ == '__main__':
 #     quote = random_python_quote()
 #     print quote
+
+# TODO
+# String reversals: reverse words, sentences
+
+# a[start:end]      # items start through end-1
+# a[start:]         # items start through the rest of the array
+# a[:end]           # items from the beginning through end-1
+# a[:]              # a copy of the whole array
+# a[start:end:step] # start through not past end, by step
+
+# That's the same as a[:] a copy of the whole list, with a :step value of -1, which results in a reversed copy of the whole list
+print(sentence[::-1])
+
+
+print(''.join(reversed(sentence)))
+
+# An interactive "mad libs" script
+# Anagram generator
