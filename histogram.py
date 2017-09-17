f670ab8cea2b3d521e1988d25da44c0c49e6bf11
Specialist17 <arocho.fernando.13@gmail.com>
2017-09-17 14:59:29 -0700

    cleanup

--- histogram.py
+++ histogram.py
@@ -3,17 +3,12 @@ import time
 import re
 from collections import Counter
 
-
-
-
 def histogram(source_text):
+    startTime = time.time()
     fh = open(source_text, "r")
     file = fh.read().lower()
     file = re.sub('[^a-z\ \']+', " ", file)
-    # print(file)
     wordList = file.split()
-    # print(len(wordList))
-    # print(wordList)
 
     word_dict = {}
     word_tuple_list = []
@@ -26,14 +21,8 @@ def histogram(source_text):
 
     sorted_word_dict = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
     word_count = unique_words(sorted_word_dict)
-    print(word_count)
-    # print(word_dict)
-    # print(sorted_word_dict)
-    # print(word_tuple_list)
     # counts = Counter(wordList)
     # elmax = counts.most_common(100)
-    # print(elmax)
-    print(sorted_word_dict)
     return sorted_word_dict
 
 
@@ -48,21 +37,7 @@ def frequency(word, histogram):
 
 
 
-my_histogram = histogram("/Users/Specialist/Desktop/GulliversTravels.txt")
+# my_histogram = histogram("/Users/Specialist/Desktop/GulliversTravels.txt")
+my_histogram = histogram("/Users/Specialist/Desktop/TheScienceOfVocalPower.txt")
 
-frequency("them", my_histogram)
-
-
-# startTime = time.time()
-# file = open('/Users/Specialist/Downloads/Ulysses_NT.txt', 'r')
-#
-# text = file.read().lower()
-# file.close()
-#
-# text = re.sub('[^a-z\ \']+', " ", text)
-# words = list(text.split())
-#
-# counts = Counter(words)
-# elmax = counts.most_common(100)
-# print(elmax)
-# print(time.time() - startTime)
+frequency("you", my_histogram)
