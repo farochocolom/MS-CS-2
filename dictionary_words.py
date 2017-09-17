f112cc2895bbe4b70ff6044aea00edae9ef348d0
Specialist17 <arocho.fernando.13@gmail.com>
2017-09-17 11:44:37 -0700

    implemented algorithm to create random sentence from words file

--- dictionary_words.py
+++ dictionary_words.py
@@ -8,20 +8,30 @@ fh = open("/usr/share/dict/words","r")
 file = fh.read()
 sentence = ""
 wordList = file.split()
-print(wordList)
-print(len(wordList))
-print(sample(wordList, 5))
+# print(wordList)
+# print(len(wordList))
+# print(sample(wordList, 5))
 
 
-def best_preserve(num):
+def create_random_sentence(num, words=wordList):
     startTime = time.time()
-    process(sample(wordList, num))
+
+    localWords = words
+    finalWordList = []
+    for word in range(0, num):
+        rand_index = random.randint(0, len(localWords) - 1)
+        finalWordList.append(localWords[rand_index])
+        localWords.pop(rand_index)
+
     print(time.time() - startTime)
+    return " ".join(finalWordList)
+
+
 
 
 def process(wordlist):
     print(" ".join(wordlist))
 
+userIn = int(input("Enter a number: "))
 
-
-best_preserve(int(input("Enter a number: ")))
+print(create_random_sentence(userIn))
