3f157d8ea78ee490eee663d850d25aec5692e289
Specialist17 <arocho.fernando.13@gmail.com>
2017-09-17 15:59:01 -0700

    got frequency using tuples

--- histogram.py
+++ histogram.py
@@ -2,27 +2,35 @@ import random
 import time
 import re
 from collections import Counter
+from operator import itemgetter
+
 
 def histogram(source_text):
-    startTime = time.time()
+    start_time = time.time()
     fh = open(source_text, "r")
     file = fh.read().lower()
     file = re.sub('[^a-z\ \']+', " ", file)
-    wordList = file.split()
+    word_list = file.split()
 
     word_dict = {}
-    word_tuple_list = []
+    # word_dict = {word: 1 if word in word_list else value+1 for word, value in word_list}
+    # word_tuple_list = []
 
-    for word in wordList:
+    for word in word_list:
         if word in word_dict:
             word_dict[word] += 1
         else:
             word_dict[word] = 1
 
-    sorted_word_dict = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
-    word_count = unique_words(sorted_word_dict)
+
+    # sorted_word_dict = sorted(word_dict.items(), key=lambda word_tuple: word_tuple[1], reverse=True)
+    sorted_word_dict = sorted(word_dict.items(), key=itemgetter(1), reverse=True)
     # counts = Counter(wordList)
     # elmax = counts.most_common(100)
+
+    print(time.time() - start_time)
+
+    print(sorted_word_dict)
     return sorted_word_dict
 
 
@@ -30,14 +38,15 @@ def unique_words(word_list):
     return len(word_list)
 
 
-
 def frequency(word, histogram):
-    word_dict = dict(histogram)
-    print(word_dict[word])
+    start_time = time.time()
+    print([x[1] for x in histogram if x[0] == word])
+    print(time.time() - start_time)
+    # word_dict = dict(histogram)
+    # print(word_dict[word])
 
 
+my_histogram = histogram("/Users/Specialist/Desktop/GulliversTravels.txt")
+# my_histogram = histogram("/Users/Specialist/Desktop/TheScienceOfVocalPower.txt")
 
-# my_histogram = histogram("/Users/Specialist/Desktop/GulliversTravels.txt")
-my_histogram = histogram("/Users/Specialist/Desktop/TheScienceOfVocalPower.txt")
-
-frequency("you", my_histogram)
+frequency("the", my_histogram)
