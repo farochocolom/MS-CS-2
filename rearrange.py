d4425d632877eedd4e521b2024b255d2e16773ea
Specialist17 <arocho.fernando.13@gmail.com>
2017-09-17 10:53:45 -0700

    created algorithm for randomizing a sentence

--- rearrange.py
+++ rearrange.py
@@ -4,17 +4,31 @@ import sys
 
 wordList = sys.argv
 del wordList[0]
-random.shuffle(wordList)
+print(wordList[2])
+# print(wordList[::-1])
+# random.shuffle(wordList)
 sentence = " ".join(wordList)
 print(sentence)
 
-# def random_python_quote():
-#     rand_index = random.randint(0, len(quotes) - 1)
-#     return quotes[rand_index]
+def random_python_quote(words):
+    localWords = words
+    finalWordList = []
+    for word in range(0, len(words)):
+        rand_index = random.randint(0, len(localWords) - 1)
+        finalWordList.append(localWords[rand_index])
+        localWords.pop(rand_index)
+    return " ".join(finalWordList)
+
+# def get_removed_element_at_index(words, index):
+
+
 #
 # if __name__ == '__main__':
 #     quote = random_python_quote()
 #     print quote
+
+
+print(random_python_quote(wordList))
 
 # TODO
 # String reversals: reverse words, sentences
@@ -27,23 +41,25 @@ print(sentence)
 
 # That's the same as a[:] a copy of the whole list, with a :step value of -1,
 # which results in a reversed copy of the whole list
-print(sentence[::-1])
-
-
-print(''.join(reversed(sentence)))
+# print(sentence[::-1])
+# print(reversed(sentence))
+# print(list(reversed(sentence)))
+# print(str(reversed(sentence)))
+#
+# print(''.join(reversed(sentence)))
 
 # An interactive "mad libs" script
 
-
-number = input("Number: ")
-profession1 = input("Enter a profession: ")
-profession2 = input("Enter another profession: ")
-city1 = input("Enter a city: ")
-name = input("Enter a name: ")
-city2 = input("Enter another city: ")
-profession3 = input("Enter another profession: ")
-bodypart = input("Enter a body part: ")
-
-print(number + " years after the end of Rush Hour 2, James Carter is no longer a " + profession1 + ", but a " + profession2 + " on the streets of " + city1 + ". Lee is now the bodyguard for his friend " + name + ". Lee is still upset with Carter about an incident in " + city2 + " when Carter accidentally shot Lee`s girlfriend, " + profession3 + "Isabella Molina, in the " + bodypart + ".")
+#
+# number = input("Number: ")
+# profession1 = input("Enter a profession: ")
+# profession2 = input("Enter another profession: ")
+# city1 = input("Enter a city: ")
+# name = input("Enter a name: ")
+# city2 = input("Enter another city: ")
+# profession3 = input("Enter another profession: ")
+# bodypart = input("Enter a body part: ")
+#
+# print(number + " years after the end of Rush Hour 2, James Carter is no longer a " + profession1 + ", but a " + profession2 + " on the streets of " + city1 + ". Lee is now the bodyguard for his friend " + name + ". Lee is still upset with Carter about an incident in " + city2 + " when Carter accidentally shot Lee`s girlfriend, " + profession3 + "Isabella Molina, in the " + bodypart + ".")
 
 # Anagram generator
