33dbc5fa7ad38dc0ce5a8a85356f071868c4b565
Specialist17 <arocho.fernando.13@gmail.com>
2017-09-26 12:31:41 -0700

    have 2 routes for generating sentences

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask
+from flask import render_template
 import stochastic_sampling
 import os
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/sentence')
 def hello_world():
     dirpath = os.getcwd()
     word_file = stochastic_sampling.process_histogram_without_numbers(
         dirpath+"/output.txt")
 
     sentence = stochastic_sampling.create_random_sentence(10, word_file)
-    return sentence
+
+    return render_template('index.html', sentence=sentence)
+
+@app.route('/sentence/<int:num>')
+def sentence_with_num_of_words(num):
+    dirpath = os.getcwd()
+    word_file = stochastic_sampling.process_histogram_without_numbers(
+        dirpath+"/output.txt")
+
+    sentence = stochastic_sampling.create_random_sentence(num, word_file)
+
+    return render_template('index.html', sentence=sentence)
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
