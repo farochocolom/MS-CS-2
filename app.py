f3e297d416e2a786a91f29ec27887aa8caee1d13
Specialist17 <arocho.fernando.13@gmail.com>
2017-09-25 21:16:47 -0700

    edited app.py

--- app.py
+++ app.py
@@ -4,15 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, mundo!'
-
-@app.route('/sentence')
-def create_sentence():
     word_file = stochastic_sampling.process_histogram_without_numbers(
         "/Users/Specialist/Documents/Code/Makeschool/CS-2_TweetGenerator/output.txt")
 
     sentence = stochastic_sampling.create_random_sentence(10, word_file)
     return sentence
+
 
 if __name__ == '__main__':
     app.run()
