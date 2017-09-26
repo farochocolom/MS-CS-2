8b7534dc9a784918ff7966220a61c21466d4de09
Specialist17 <arocho.fernando.13@gmail.com>
2017-09-25 21:37:14 -0700

    relative path for word file

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 import stochastic_sampling
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    dirpath = os.getcwd()
     word_file = stochastic_sampling.process_histogram_without_numbers(
-        "/Users/Specialist/Documents/Code/Makeschool/CS-2_TweetGenerator/output.txt")
+        dirpath+"/output.txt")
 
     sentence = stochastic_sampling.create_random_sentence(10, word_file)
     return sentence
