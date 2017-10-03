3fd1c82bb476854c13e6281c993a976c90da48e5
Specialist17 <arocho.fernando.13@gmail.com>
2017-10-02 17:33:29 -0700

    refactoring

---
+++ Tweet_Generator/tokenize.py
@@ -0,0 +1,13 @@
+
+
+def histogram_dict(word_list):
+
+    word_dict = {}
+
+    for word in word_list:
+        if word in word_dict:
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
+
+    return word_dict
