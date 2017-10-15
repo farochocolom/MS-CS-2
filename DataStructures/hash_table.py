7a82f8046def35cda1c62d8502f7a182c3f4e7ee
Specialist17 <arocho.fernando.13@gmail.com>
2017-10-15 14:26:49 -0700

    changes

--- DataStructures/hash_table.py
+++ DataStructures/hash_table.py
@@ -5,88 +5,97 @@ from linkedList import LinkedList
 
 class HashTable(object):
 
-    def __init__(self, init_size=8):
+    def __init__(self, init_size=8):    # Linear time
         """Initialize this hash table with the given initial size"""
         self.buckets = [LinkedList() for i in range(init_size)]
+        # List comprehension with empty LinkedList initializer, linear time
 
-    def __str__(self):
+    def __str__(self):  # linear time
         """Return a formatted string representation of this hash table"""
+        # list comprehension for items in hash table, linear time
         items = ['{}: {}'.format(repr(k), repr(v)) for k, v in self.items()]
+
+        # String joining, linear time
         return '{' + ', '.join(items) + '}'
 
-    def __repr__(self):
+    def __repr__(self):     # linear time
         """Return a string representation of this hash table"""
+        # Return all the items in the hash table in a string representation, linear time
         return 'HashTable({})'.format(repr(self.items()))
 
-    def __iter__(self):
-        current = self.buckets[0]
-        for index in range(0, self.length):
-            yield current
-            current = current[index+1]
+    # def __iter__(self):
+    #     current = self.buckets[0]
+    #     for index in range(0, self.length()):
+    #         yield current
+    #         current = current[index+1]
+    #         print(current)
 
-    def _bucket_index(self, key):
+    def _bucket_index(self, key):   # constant time
         """Return the bucket index where the given key would be stored"""
         return hash(key) % len(self.buckets)
+        # both hash and len are constant time.
+        # In python, a list knows it's own length
 
-    def keys(self):
+    def keys(self):     # n^2 time
         """Return a list of all keys in this hash table"""
         # Collect all keys in each of the buckets
-        all_keys = []
-        for bucket in self.buckets:
-            for key, value in bucket.items():
-                all_keys.append(key)
-        return all_keys
+        all_keys = []   # create empty list, constant time
+        for bucket in self.buckets:     # iterate through every bucket, linear time
+            for key, value in bucket.items():   # iterate through every tuple in the bucket, linear time
+                all_keys.append(key)    # append to the list, constant time
+        return all_keys     # return all_keys list, constant times
 
-    def values(self):
+    def values(self):   # n^2 time
         """Return a list of all values in this hash table"""
-        all_values = []
-        for bucket in self.buckets:
-            print("bucket:" + str(bucket))
-            for key, value in bucket.items():
-                all_values.append(value)
-        return all_values
+        all_values = []     # create empty list, constant time
+        for bucket in self.buckets:     # iterate through every bucket, linear time
+            for key, value in bucket.items():   # iterate through every tuple in the bucket, linear time
+                all_values.append(value)     # append to the list, constant time
+        return all_values   # return all_values list, constant times
 
-    def items(self):
+    def items(self):    # linear time
         """Return a list of all items (key-value pairs) in this hash table"""
-        all_items = []
-        for bucket in self.buckets:
-            all_items.extend(bucket.items())
-        return all_items
+        all_items = []  # create empty list, constant time
+        for bucket in self.buckets:     # iterate through every bucket, linear time
+            all_items.extend(bucket.items())    # append to the list, constant time
+        return all_items    # return all_items list, constant times
 
-    def length(self):
+    def length(self):   # linear time
         """Return the length of this hash table by traversing its buckets"""
-        count = 0
-        for bucket in self.buckets:
-            count += len(bucket.items())
-        return count
+        count = 0   # create and set count variable to 0, constant time
+        for bucket in self.buckets:     # iterate through every bucket, linear time
+            count += len(bucket.items())    # add 1 to count, constant time
+        return count    # return count, constant time
 
-    def contains(self, key):
+    def contains(self, key):    # constant time
         """Return True if this hash table contains the given key, or False"""
-        key_hash = self._bucket_index(key)
+        key_hash = self._bucket_index(key)  # bucket_index is constant time, this is constant time
 
-        if self.buckets[key_hash] is not None:
-            node = self.buckets[key_hash]
+        if self.buckets[key_hash] is not None:  # check if the index is empty, constant time
+            node = self.buckets[key_hash]   # set node to the index value, constant time
+            # check if node is empty, and that the key is equal to the node data, constant time
             if not node.is_empty() and node.head.data[0] == key:
-                return True
+                return True  # constant time
             else:
-                return False
+                return False  # constant time
 
     def get(self, key):
         """Return the value associated with the given key, or raise KeyError"""
-        key_hash = self._bucket_index(key)
+        key_hash = self._bucket_index(key)  # bucket_index is constant time, this is constant time
 
-        if self.buckets[key_hash] is not None:
-            node = self.buckets[key_hash]
+        if self.buckets[key_hash] is not None:  # check if the index is empty, constant time
+            node = self.buckets[key_hash]   # set node to the index value, constant time
+            # check if node is empty, and that the key is equal to the node data, constant time
             if not node.is_empty() and node.head.data[0] == key:
-                return node.head.data[1]
+                return node.head.data[1]    # constant time
             else:
-                raise KeyError('Key does not exist')
+                raise KeyError('Key does not exist')    # constant time
 
     def set(self, key, value):
         """Insert or update the given key with its associated value"""
-        key_hash = self._bucket_index(key)
-        key_value = key, value
-        if self.buckets[key_hash].is_empty():
+        key_hash = self._bucket_index(key)  # set the key hash to the bucket index
+        key_value = key, value  # set the key value to a tuple containing the key and value
+        if self.buckets[key_hash].is_empty():  # 
             self.buckets[key_hash] = LinkedList([key_value])
         else:
             if self.contains(key):
@@ -119,8 +128,8 @@ def test_hash_table():
     print('get(X): ' + str(ht.get('X')))
     print('length: ' + str(ht.length()))
 
-    # for item in ht:
-    #     print(item)
+    for item in ht:
+        print(ht[item])
 
     # Enable this after implementing delete:
     print('Deleting entries:')
