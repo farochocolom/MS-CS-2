afae75441ba6ef0797df88308f0405919a9299c3
Specialist17 <arocho.fernando.13@gmail.com>
2017-10-17 09:11:10 -0700

    corpus generating script created

--- app.py
+++ app.py
@@ -23,23 +23,36 @@ def hello_world():
     sentence = markov.walk_the_markov(15, weighted)
 
     return render_template('index.html', sentence=sentence)
-    return "Hello"
 
 
 @app.route('/corpus_creator')
 def create_the_corpus():
+
     file_to_read = open("./pages.txt", "r")
+    file_to_write = open("./corpus.txt", "w")
     read_file = file_to_read.read()
     page_list = read_file.split()
 
     for page in page_list:
         print(page)
+        resp = requests.get("https://api.diffbot.com/v3/article?token=257975cb7afce09e99c068569f3f19fa&url="+page)
+        sentence = resp.content.decode()
+        # print(sentence)
+        text = json.loads(sentence)
+        file_to_write.write(text['objects'][0]['text'])
 
-    resp = requests.get("https://api.diffbot.com/v3/article?token=257975cb7afce09e99c068569f3f19fa&url=https://www.scienceofpeople.com/2017/09/what-is-an-intrapreneur/")
-    # sentence = resp.content
-    sentence = resp.content.decode()
-    text = json.loads(sentence)
-    return text['objects'][0]['text']
+    file_to_write.close()
+
+    # file_to_read = open("./pages.txt", "r")
+    # read_file = file_to_read.read()
+    # page_list = read_file.split()
+    # print(page_list)
+    #
+    # resp = requests.get("https://api.diffbot.com/v3/article?token=257975cb7afce09e99c068569f3f19fa&url=https://www.scienceofpeople.com/2017/09/what-is-an-intrapreneur/")
+    # # sentence = resp.content
+    # sentence = resp.content.decode()
+    # text = json.loads(sentence)
+    return "tamo xóticos"
     # return render_template('index.html', sentence=sentence)
 
 
