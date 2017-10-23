26d8503de9bc9516659a2b30810562e2050a42c4
Specialist17 <arocho.fernando.13@gmail.com>
2017-10-22 17:28:47 -0700

    favorites button working

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
+from flask_restful import Api
 from flask import render_template
-import stochastic_sampling
+from pymongo import MongoClient
 import tokenize_corpus
-import os
 import markov
-import requests
-import json
-from pprint import pprint
+
+
 app = Flask(__name__)
+# app.config.from_pyfile('config.cfg')
+mongo = MongoClient('mongodb://Specialist17:darkknight17@ds227555.mlab.com:27555/tweet_gen')
+app.db = mongo.tweet_gen
+api = Api(app)
 
 
 @app.route('/')
@@ -17,7 +20,30 @@ def hello_world():
     weighted = markov.weighted_markov(markov_chain_var)
     sentence = markov.walk_the_markov(weighted)
     return render_template('index.html', sentence=sentence)
-    
+
+
+@app.route('/favorites', methods=['POST', 'GET'])
+def favorites():
+    favorites_col = app.db.favorites
+    if request.method == 'POST':
+        sentence = request.form['sentence']
+        favorites_col.insert_one({'sentence': sentence})
+        return redirect('/')
+    else:
+        favorites = favorites_col.find()
+        fav_array = []
+        for favorite in favorites:
+            fav_array.append(favorite)
+        return render_template('favorites.html', favorites=fav_array)
+
+
+#  Custom JSON serializer for flask_restful
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+    resp = make_response(JSONEncoder().encode(data), code)
+    resp.headers.extend(headers or {})
+    return resp
+
 
 if __name__ == '__main__':
     app.run()
